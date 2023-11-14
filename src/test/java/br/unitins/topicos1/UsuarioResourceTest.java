194d89af65b574f1e71a3bd7af7c612b05edceb5
João Pedro de Oliveira Paulino <125329586+JoaoPOPaulino@users.noreply.github.com>
2023-11-14 14:55:08 -0300

    att

--- src/test/java/br/unitins/topicos1/UsuarioResourceTest.java
+++ src/test/java/br/unitins/topicos1/UsuarioResourceTest.java
@@ -32,93 +32,89 @@ public class UsuarioResourceTest {
                                 .statusCode(200);
         }
 
-        // @Test
-        // public void testInsert() {
-        // List<TelefoneDTO> telefones = new ArrayList<TelefoneDTO>();
-        // telefones.add(new TelefoneDTO("63", "5555-5555"));
+        @Test
+        public void testInsert() {
+                List<TelefoneDTO> telefones = new ArrayList<TelefoneDTO>();
+                telefones.add(new TelefoneDTO("63", "5555-5555"));
 
-        // EnderecoDTO endereco = new EnderecoDTO("Estado", "Cidade", "Quadra", "Rua",
-        // 123);
+                EnderecoDTO endereco = new EnderecoDTO("Estado", "Cidade", "Quadra", "Rua", 123);
 
-        // UsuarioDTO dto = new UsuarioDTO(
-        // "Mark Zuckerberg Insert",
-        // "marquinho",
-        // "333",
-        // 1,
-        // telefones,
-        // endereco);
+                UsuarioDTO dto = new UsuarioDTO(
+                                "Mark Zuckerberg Insert",
+                                "marquinho",
+                                "333",
+                                1,
+                                telefones,
+                                endereco);
 
-        // given()
-        // .contentType(ContentType.JSON)
-        // .body(dto)
-        // .when().post("/usuarios")
-        // .then()
-        // .statusCode(201)
-        // .body(
-        // "id", notNullValue(),
-        // "nome", is("Mark Zuckerberg Insert"),
-        // "login", is("marquinho"));
-        // }
+                given()
+                                .contentType(ContentType.JSON)
+                                .body(dto)
+                                .when().post("/usuarios")
+                                .then()
+                                .statusCode(201)
+                                .body(
+                                                "id", notNullValue(),
+                                                "nome", is("Mark Zuckerberg Insert"),
+                                                "login", is("marquinho"));
+        }
 
-        // @Test
-        // public void testUpdate() {
-        // List<TelefoneDTO> telefones = new ArrayList<TelefoneDTO>();
-        // telefones.add(new TelefoneDTO("63", "5555-5555"));
+        @Test
+        public void testUpdate() {
+                List<TelefoneDTO> telefones = new ArrayList<TelefoneDTO>();
+                telefones.add(new TelefoneDTO("63", "5555-5555"));
 
-        // EnderecoDTO endereco = new EnderecoDTO("Estado", "Cidade", "Quadra", "Rua",
-        // 123);
+                EnderecoDTO endereco = new EnderecoDTO("Estado", "Cidade", "Quadra", "Rua", 123);
 
-        // UsuarioDTO dtoInsert = new UsuarioDTO(
-        // "Mark Zuckerberg Update",
-        // "morkos",
-        // "333",
-        // 1,
-        // telefones,
-        // endereco);
+                UsuarioDTO dtoInsert = new UsuarioDTO(
+                                "Mark Zuckerberg Update",
+                                "morkos",
+                                "333",
+                                1,
+                                telefones,
+                                endereco);
 
-        // UsuarioResponseDTO usuarioTest = usuarioService.insert(dtoInsert);
+                UsuarioResponseDTO usuarioTest = usuarioService.insert(dtoInsert);
 
-        // Long id = usuarioTest.id();
+                Long id = usuarioTest.id();
 
-        // UsuarioDTO dtoUpdate = new UsuarioDTO(
-        // "Mark Zuckerberg",
-        // "mark",
-        // "555",
-        // 1,
-        // telefones,
-        // endereco);
+                UsuarioDTO dtoUpdate = new UsuarioDTO(
+                                "Mark Zuckerberg",
+                                "mark",
+                                "555",
+                                1,
+                                telefones,
+                                endereco);
 
-        // given()
-        // .contentType(ContentType.JSON)
-        // .body(dtoUpdate)
-        // .when().put("/usuarios/" + id) // Use o ID obtido para formar o URL de
-        // atualização
-        // .then()
-        // .statusCode(204);
-        // }
+                given()
+                                .contentType(ContentType.JSON)
+                                .body(dtoUpdate)
+                                .when().put("/usuarios/" + id) // Use o ID obtido para formar o URL de atualização
+                                .then()
+                                .statusCode(204);
+        }
 
-        // @Test
-        // public void testDelete() {
-        // List<TelefoneDTO> telefones = new ArrayList<TelefoneDTO>();
-        // telefones.add(new TelefoneDTO("63", "5555-5555"));
+        @Test
+        public void testDelete() {
+                List<TelefoneDTO> telefones = new ArrayList<TelefoneDTO>();
+                telefones.add(new TelefoneDTO("63", "5555-5555"));
 
-        // EnderecoDTO endereco = new EnderecoDTO("Estado", "Cidade", "Quadra", "Rua",
-        // 123);
+                EnderecoDTO endereco = new EnderecoDTO("Estado", "Cidade", "Quadra", "Rua", 123);
 
-        // UsuarioDTO dto = new UsuarioDTO(
-        // "Mark Zuckerberg Delete",
-        // "marquinho",
-        // "333",
-        // 1,
-        // telefones,
-        // endereco);
+                UsuarioDTO dto = new UsuarioDTO(
+                                "Mark Zuckerberg Delete",
+                                "marquinho",
+                                "333",
+                                1,
+                                telefones,
+                                endereco);
 
-        // UsuarioResponseDTO usuarioTest = usuarioService.insert(dto);
-        // Long id = usuarioTest.id();
+                UsuarioResponseDTO usuarioTest = usuarioService.insert(dto);
+                Long id = usuarioTest.id();
 
-        // given()
-        // .when().delete("/usuarios/" + id)
-        // .then()
-        // .statusCode(204);
-        // }
+                given()
+                                .when().delete("/usuarios/" + id)
+                                .then()
+                                .statusCode(204);
+        }
 }
