ca390039679f813253d4cd352ad8f38a468afb5c
João Pedro de Oliveira Paulino <125329586+JoaoPOPaulino@users.noreply.github.com>
2023-12-01 22:58:10 -0300

    Att

--- src/test/java/br/unitins/topicos1/UsuarioResourceTest.java
+++ src/test/java/br/unitins/topicos1/UsuarioResourceTest.java
@@ -3,7 +3,6 @@ package br.unitins.topicos1;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
