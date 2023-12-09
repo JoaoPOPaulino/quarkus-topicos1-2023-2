4b3c3aa84b33841fdde562ac5186d46598949aac
João Pedro de Oliveira Paulino <125329586+JoaoPOPaulino@users.noreply.github.com>
2023-12-09 18:47:05 -0300

    Att

--- src/main/java/br/unitins/topicos1/resource/ReservaResource.java
+++ src/main/java/br/unitins/topicos1/resource/ReservaResource.java
@@ -7,6 +7,7 @@ import org.jboss.logging.Logger;
 
 import br.unitins.topicos1.dto.reserva.ReservaDTO;
 import br.unitins.topicos1.dto.reserva.ReservaResponseDTO;
+import br.unitins.topicos1.model.Reserva;
 import br.unitins.topicos1.service.reserva.ReservaService;
 import io.smallrye.jwt.build.JwtException;
 import jakarta.annotation.security.RolesAllowed;
@@ -18,6 +19,7 @@ import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
@@ -43,30 +45,78 @@ public class ReservaResource {
     @POST
     @RolesAllowed({ "User", "Admin" })
     public Response insert(@Valid ReservaDTO dto) {
-        LOGGER.info("Iniciando a criação de uma nova reserva com data de início: " + dto.dataInicio());
-        Response response = Response.status(Status.CREATED).entity(service.insert(dto)).build();
-        LOGGER.info("Reserva criada com sucesso");
-        return response;
+        try {
+            Long usuarioId = Long.parseLong(jwt.getSubject());
+            ReservaDTO novaDto = new ReservaDTO(dto.dataInicio(), dto.dataFim(), dto.idQuarto(), usuarioId);
+
+            LOGGER.info("Criando reserva para o usuário ID: " + usuarioId);
+            ReservaResponseDTO reservaCriada = service.insert(novaDto);
+            LOGGER.info("Reserva criada com sucesso para o usuário ID: " + usuarioId);
+
+            return Response.status(Status.CREATED).entity(reservaCriada).build();
+        } catch (NumberFormatException e) {
+            LOGGER.error("Erro na conversão do ID do usuário: " + e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity("Formato de ID inválido.").build();
+        } catch (Exception e) {
+            LOGGER.error("Erro ao criar reserva: " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Erro ao criar reserva.").build();
+        }
     }
 
     @PUT
     @Path("/{id}")
+    @Transactional
     @RolesAllowed({ "User", "Admin" })
     public Response update(@Valid ReservaDTO dto, @PathParam("id") Long id) {
-        LOGGER.info("Iniciando atualização da reserva com ID: " + id);
-        service.update(dto, id);
-        LOGGER.info("Reserva com ID: " + id + " atualizada com sucesso");
-        return Response.noContent().build();
+        try {
+            Long usuarioId = Long.parseLong(jwt.getSubject());
+            ReservaDTO novaDto = new ReservaDTO(dto.dataInicio(), dto.dataFim(), dto.idQuarto(), usuarioId);
+
+            LOGGER.info("Atualizando reserva para o usuário ID: " + usuarioId);
+            ReservaResponseDTO reservaAtualizada = service.update(novaDto, id);
+            LOGGER.info("Reserva com ID: " + id + " atualizada com sucesso para o usuário ID: " + usuarioId);
+
+            return Response.status(Status.NO_CONTENT).build();
+        } catch (NumberFormatException e) {
+            LOGGER.error("Erro na conversão do ID do usuário: " + e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity("Formato de ID inválido.").build();
+        } catch (NotFoundException e) {
+            LOGGER.error("Reserva não encontrada: " + e.getMessage());
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } catch (Exception e) {
+            LOGGER.error("Erro ao atualizar reserva: " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Erro interno do servidor.").build();
+        }
     }
 
     @DELETE
     @Path("/{id}")
     @RolesAllowed({ "User", "Admin" })
     public Response delete(@PathParam("id") Long id) {
-        LOGGER.info("Iniciando exclusão da reserva com ID: " + id);
-        service.delete(id);
-        LOGGER.info("Reserva com ID: " + id + " excluída com sucesso");
-        return Response.noContent().build();
+        try {
+            Long usuarioId = Long.parseLong(jwt.getSubject());
+            ReservaResponseDTO reservaDTO = service.findById(id);
+
+            if (reservaDTO == null) {
+                LOGGER.error("Reserva não encontrada.");
+                return Response.status(Status.NOT_FOUND).build();
+            }
+
+            if (!reservaDTO.usuario().id().equals(usuarioId) && !jwt.getGroups().contains("Admin")) {
+                LOGGER.error("Usuário não autorizado a deletar esta reserva.");
+                return Response.status(Status.FORBIDDEN).build();
+            }
+
+            service.delete(id);
+            LOGGER.info("Reserva com ID: " + id + " excluída com sucesso.");
+            return Response.noContent().build();
+        } catch (NumberFormatException e) {
+            LOGGER.error("Erro na conversão do ID do usuário: " + e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity("Formato de ID inválido.").build();
+        } catch (Exception e) {
+            LOGGER.error("Erro ao excluir reserva: " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Erro interno do servidor.").build();
+        }
     }
 
     @GET
