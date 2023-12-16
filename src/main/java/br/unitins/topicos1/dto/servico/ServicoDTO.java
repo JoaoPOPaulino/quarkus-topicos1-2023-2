976090c5eaa609a1464a2d63c0aec6ef52a6be86
João Pedro de Oliveira Paulino <125329586+JoaoPOPaulino@users.noreply.github.com>
2023-12-15 22:37:27 -0300

    Att

--- src/main/java/br/unitins/topicos1/dto/servico/ServicoDTO.java
+++ src/main/java/br/unitins/topicos1/dto/servico/ServicoDTO.java
@@ -6,14 +6,12 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record ServicoDTO(
-        @NotNull(message = "O nome não pode ser nulo.") @Pattern(regexp = "^[A-Za-z ]+$", message = "O nome deve conter apenas letras e espaços.") String nome,
+                @NotNull(message = "O nome não pode ser nulo.") @Pattern(regexp = "^[A-Za-zÀ-ÖØ-öø-ÿ ]+$", message = "O nome deve conter apenas letras e espaços.") String nome,
 
-        @NotNull(message = "A descrição não pode ser nula") @Pattern(regexp = "^[A-Za-z0-9 ]+$", message = "A descrição deve conter apenas letras, números e espaços.") String descricao,
+                @NotNull(message = "A descrição não pode ser nula") @Pattern(regexp = "^[A-Za-z0-9À-ÖØ-öø-ÿ ]+$", message = "A descrição deve conter apenas letras, números e espaços.") String descricao,
 
-        @NotNull(message = "A hora inicial não pode ser nula.") LocalTime horaI,
+                @NotNull(message = "A hora inicial não pode ser nula.") LocalTime horaI,
 
-        @NotNull(message = "A hora final não pode ser nula.") LocalTime horaF
-
-) {
+                @NotNull(message = "A hora final não pode ser nula.") LocalTime horaF) {
 
 }
